82fd60a799c8ba6f7dd3a935028ea40cee62074b
Jeremy Niu <a18201979707@163.com>
2021-12-12 14:46:24 +0800

    create demo4.go and this is array exercises

---
+++ exercises/demo4.go
@@ -0,0 +1,16 @@
+//求数组[1, 3, 5, 7, 8]所有元素的和
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var elementSum int
+	elementArray := []int{1, 3, 5, 7, 8}
+	for _, element := range elementArray {
+		elementSum += element
+	}
+	fmt.Printf("数组中所有元素的和=%d\n", elementSum)
+}
